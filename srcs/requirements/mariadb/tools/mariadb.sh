61b10c9761024cca59deb335fdbf9901be97bf55
Youssef Achehboune <achehbun949@gmail.com>
2022-12-15 08:21:47 +0100

    set password for root mysql

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,6 +1,12 @@
 # !bin/bash
 
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS  ${DATABASE};"
-mysql -u root -e "CREATE USER '${USER_NAME}'@'%' IDENTIFIED BY '${USER_PASSWORD}';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON ${DATABASE}.* TO '${USER_NAME}'@'%';"
-mysql -u root -e "FLUSH PRIVILEGES;"
+
+mysql -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${ROOT_PASSWORD}');"
+mysql -uroot -p$ROOT_PASSWORD -e "show databases;"
+
+
+mysql -u root -p$ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS  ${DATABASE};"
+mysql -u root -p$ROOT_PASSWORD -e "CREATE USER '${USER_NAME}'@'%' IDENTIFIED BY '${USER_PASSWORD}';"
+mysql -u root -p$ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON ${DATABASE}.* TO '${USER_NAME}'@'%';"
+mysql -u root -p$ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+mysql -u root -p$ROOT_PASSWORD -e "show databases;"
