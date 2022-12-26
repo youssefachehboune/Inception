d741910c0532c5305d4ce7ce111d6978fc3ec438
Youssef Achehboune <achehbun949@gmail.com>
2022-12-26 20:01:57 +0100

    Update the domain name

--- srcs/requirements/bonus/redis/script.sh
+++ srcs/requirements/bonus/redis/script.sh
@@ -5,7 +5,6 @@ if [ ! -f "/etc/redis/redis.conf.bak" ]; then
     cp /etc//redis/redis.conf /etc/redis/redis.conf.bak
 
     sed -i "s|127.0.0.1|0.0.0.0|g" /etc/redis/redis.conf
-    # sed -i "s|# requirepass foobared|requirepass $REDIS_PWD|g" /etc/redis.conf
     sed -i "s|# maxmemory <bytes>|maxmemory 256mb|g" /etc/redis/redis.conf
     sed -i "s|# maxmemory-policy noeviction|maxmemory-policy allkeys-lru|g" /etc/redis/redis.conf
 
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -3,7 +3,7 @@
 # if [ ! -e /var/www/html/wordpress/wp-config.php ]; then
     wp config create	--allow-root --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --dbhost=${DB_HOST} --path='/var/www/html/wordpress'
     sleep 5
-    wp core install --url=${WP_URL_1} --title=${WP_TITLE} --admin_user=${WP_ROOT_USER} --admin_password=${WP_ROOT_PASSWORD} --admin_email=${WP_ROOT_EMAIL} --path='/var/www/html/wordpress' --allow-root
+    wp core install --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ROOT_USER} --admin_password=${WP_ROOT_PASSWORD} --admin_email=${WP_ROOT_EMAIL} --path='/var/www/html/wordpress' --allow-root
     wp user create ${WP_USER} ${WP_USER_EMAIL}  --user_pass=${WP_PASSWORD} --role=author --path='/var/www/html/wordpress' --allow-root
     wp config set WP_REDIS_HOST redis --allow-root  --path='/var/www/html/wordpress'
     service php7.3-fpm start
