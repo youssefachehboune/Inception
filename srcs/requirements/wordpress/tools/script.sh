e928de1fd4089fdd54fa05d4b89beee753b93bbc
Youssef Achehboune <yachehbo@student.1337.ma>
2022-12-28 21:21:58 +0100

    Update wordpress

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -6,12 +6,10 @@ if [ ! -e /var/www/html/wordpress/wp-config.php ]; then
     wp core install --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ROOT_USER} --admin_password=${WP_ROOT_PASSWORD} --admin_email=${WP_ROOT_EMAIL} --path='/var/www/html/wordpress' --allow-root
     wp user create ${WP_USER} ${WP_USER_EMAIL}  --user_pass=${WP_PASSWORD} --role=author --path='/var/www/html/wordpress' --allow-root
     wp config set WP_REDIS_HOST redis --allow-root  --path='/var/www/html/wordpress'
-    
-    service php7.3-fpm start
+
 	wp plugin install redis-cache --allow-root --path='/var/www/html/wordpress'
     wp plugin activate redis-cache --allow-root --path='/var/www/html/wordpress'
 	wp redis enable --allow-root --path='/var/www/html/wordpress'
-    service php7.3-fpm stop
 fi
 
 mkdir -p ./run/php/
